db86ce0689e979d2991b0a199efd39ad967dbaf7
Bruno Louzada <blouzada@live.com>
2018-04-19 01:41:23 -0300

    fix teste

--- src/components/__tests__/search_bar.test.js
+++ src/components/__tests__/search_bar.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { shallow, mount } from 'enzyme';
 import SearchBar from "../search_bar";
 
-test('Render SearchBar with input', () => {  
+test('Render SearchBar with input', () => {
   const mockVideoSearch = jest.fn()
   const searchBar = shallow(<SearchBar onSearchTermChange={mockVideoSearch}/>);
-  expect(searchBar.find('input').props().value).toEqual('surfboards');
+  expect(searchBar.find('input').props().value).toEqual('reactjs');
 });
 
 test('Should update SearchBar state term', () => {
@@ -17,7 +17,7 @@ test('Should update SearchBar state term', () => {
 
 test('Should call videoSearch with term', () => {
   const mockVideoSearch = jest.fn()
-  const searchBar = shallow(<SearchBar onSearchTermChange={mockVideoSearch}/>);  
+  const searchBar = shallow(<SearchBar onSearchTermChange={mockVideoSearch}/>);
   searchBar.find('input').simulate('change', {target: {value: 'My new value'}});
   expect(mockVideoSearch.mock.calls[1][0]).toBe('My new value');
 });
